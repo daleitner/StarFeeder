e664d2352f09f888727ad63fbff2ff3ad83b6e12
elpatzo <elpatzo@github.com>
2017-12-15 23:25:24 +0100

    script_movement

---
+++ Starfeeder/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float speed;
+    private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        float moveHorizantal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizantal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * speed);
+
+
+    }
+}
